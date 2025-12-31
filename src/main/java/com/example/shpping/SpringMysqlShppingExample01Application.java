d6f71eafee3071d9df663b47ee26f6d83d708c28
kmg <deepestdark@gmail.com>
2025-12-31 09:04:15 +0900

    project setting

---
+++ src/main/java/com/example/shpping/SpringMysqlShppingExample01Application.java
@@ -0,0 +1,13 @@
+package com.example.shpping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlShppingExample01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlShppingExample01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shpping/SpringMysqlShppingExample01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shpping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlShppingExample01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
